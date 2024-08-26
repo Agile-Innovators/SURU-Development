2607204485b5f2650db5eaec664b77b7906b99db
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-08-26 15:59:03 -0600

    tailwind local variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:
+      {
+        primary: ['Poppins', 'sans-serif']
+      },
+      colors:{
+        primary: '#0A1029',
+        secondary: '#003C55',
+        'light-blue': '#20B7DD',
+        white: '#F6F6F6',
+        grey: '#6D7585',
+        'light-grey': '#C3C8D2'
+      }
+    },
   },
   plugins: [],
 }
