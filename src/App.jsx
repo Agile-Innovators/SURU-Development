13498922c8c3a7a771546703a33e994e945f32f5
cap1112 <angelv.salazar11@gmail.com>
2024-08-26 20:46:52 -0600

    Agregar estilos a las etiquetas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
+            <h1>H1 - Hello World</h1>
+            <h2>H2 -Hello World</h2>
+            <h3>H3 -Hello World</h3>
+            <h4>H4 -Hello World</h4>
+            <span>Span -Hello World</span>
+            <p>p - Hello World</p>                
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,57 @@ export default {
         grey: '#6D7585',
         'light-grey': '#C3C8D2'
       }
+
+      
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      addBase({
+        'h1': { 
+          fontSize: '2.625rem',
+          color: theme('colors.primary'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '700', //Bold
+        },
+        'h2': { 
+          fontSize: '1.5rem', //24px
+          color: theme('colors.primary'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '600', //Semi-bold
+        },
+        'h3': { 
+          fontSize: '1.25rem', //20px
+          color: theme('colors.primary'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '600', //Semi-bold
+        },
+        'h4': { 
+          fontSize: '1.125rem', //18px
+          color: theme('colors.primary'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '600', //Semi-bold
+        },        
+        'h5': { 
+          fontSize: '1rem', //16px
+          color: theme('colors.primary'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '400', //Regular
+        },
+        'span': { 
+          fontSize: '1rem', //16px
+          color: theme('colors.primary'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '400', //Regular
+        },                
+        'p': { 
+          fontSize: '1rem', //16px
+          color: theme('colors.light-grey'),
+          fontFamily: theme('fontFamily.primary'),
+          fontWeight: '300', //regular
+        },  
+      })
+    }
+  ],
 }
 
