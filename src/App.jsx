1c9dbf54fa420b197e391025c76940ec732ea98f
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-08-26 23:18:33 -0600

    components: input, buttons, basic card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { NavBar } from "./assets/components/NavBar";
 import { PrimaryButton } from "./assets/components/PrimaryButton";
+import { SecondaryButton } from "./assets/components/SecondaryButton";
+import { Input } from "./assets/components/Input";
+import { BasicCard } from "./assets/components/BasicCard";
 
 function App() {
     return (
         <section className="max-w-screen-lg m-auto">
             <NavBar />
-            <PrimaryButton  text={"Click"}/>
+            <div className="flex gap-2">
+                <PrimaryButton text={"Click"} />
+                <SecondaryButton text={"Click2"} />
+                <form>
+                    <Input inputName={'Email'} inputId={"input-email"} type="email" labelText="Email" required={true}/>
+                    <Input inputName={'Password'} inputId={"input-password"} type="password" labelText="Password" required={true}  />
+                    <PrimaryButton text={"Click"} type="submit"/>
+                </form>
+
+                <BasicCard/>
+
+                <BasicCard>
+                    <PrimaryButton text={"Click"} />
+                </BasicCard>
+
+            </div>
         </section>
     );
 }
--- src/assets/components/PrimaryButton.jsx
+++ src/assets/components/PrimaryButton.jsx
@@ -1,7 +1,16 @@
-export function PrimaryButton({text}){
+import { PropTypes } from 'prop-types';
+
+export function PrimaryButton({text, type = 'button',  customClass = ""}){
+
     return(
-        <button className="px-8 py-3 rounded-md bg-secondary text-white m-auto">
+        <button type={`${type}`} className={`px-8 py-3 rounded-md bg-secondary text-white m-auto transition-colors duration-150 hover:bg-light-blue hover:text-primary ${customClass}`}>
             {text}
         </button>
     )
+}
+
+PrimaryButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    customClass: PropTypes.string
 }
