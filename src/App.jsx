7087eb8621fb5c5be371dc5584e99edbaad565ef
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-08-27 21:54:19 -0600

    reconfigured buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { NavBar } from "./assets/components/NavBar";
-import { PrimaryButton } from "./assets/components/PrimaryButton";
-import { SecondaryButton } from "./assets/components/SecondaryButton";
 import { Input } from "./assets/components/Input";
 import { BasicCard } from "./assets/components/BasicCard";
 import { AdvancedCard } from './assets/components/Advancedcard';
+import { ActionButton } from "./assets/components/ActionButton";
+import { RedirectButton } from "./assets/components/RedirectButton";
 
 
 
@@ -12,8 +12,6 @@ function App() {
         <section className="max-w-screen-lg m-auto">
             <NavBar />
             <div className="flex gap-2">
-                <PrimaryButton text={"Click"} />
-                <SecondaryButton text={"Click2"} />
                 <form>
                     <Input
                         inputName={"Email"}
@@ -29,23 +27,32 @@ function App() {
                         labelText="Password"
                         required={true}
                     />
-                    <PrimaryButton text={"Click"} type="submit" />
+                    <ActionButton text={"Click"} type={"submit"} variant="border"/>
                 </form>
             </div>
             <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] mt-20 gap-4">
                 {/* <BasicCard /> */}
 
                 <BasicCard>
-                    <PrimaryButton text={"Click"} />
+                    <RedirectButton text={'Click'} variant="border" href="/test"/>
                 </BasicCard>
 
                 <AdvancedCard srcImage={"../public/images/house-image.png"}>
-                    <PrimaryButton text={"Click"} />
+                    
                 </AdvancedCard>
                 <AdvancedCard srcImage={"../public/images/house-image.png"}>
-                    <PrimaryButton text={"Click"} />
+                    
                 </AdvancedCard>
+                <Input
+                        inputName={"btn"}
+                        inputId={"btn"}
+                        labelText="Password"
+                        placeholder="password"
+                        type={"password"}
+                    />
+                
             </div>
+            <RedirectButton text={'Click'} variant="fill" href="/test"/>
         </section>
     );
 }
--- src/assets/components/Input.jsx
+++ src/assets/components/Input.jsx
@@ -4,11 +4,11 @@ import { PropTypes } from 'prop-types';
 export function Input({inputName, placeholder="", type = 'text', spanText="", labelText,inputId, required = false}){
     return(
         <div className="flex flex-col">
-            <label htmlFor={`${inputId}`} className="span">{labelText}</label>
+            <label htmlFor={inputId} className="span">{labelText}</label>
             {/* se aplica una condicional para activar el atributo require del input */}
             {/* si el parametro required es true se activa el atributo */}
             {/* "&&" operador logico de jS que permite hacer evaluaciones condicionales*/}
-            <input {...(required && { required: true })} type={`${type}`} id={`${inputId}`} name={`${inputName}`} placeholder={`${placeholder}`} className="border border-light-grey rounded-md min-h-8 px-4 py-2 mt-2 focus:outline-light-blue"/>
+            <input {...(required && { required: true })} type={type} id={inputId} name={inputName} placeholder={placeholder} className="border border-light-grey rounded-md min-h-8 px-4 py-2 mt-2 focus:outline-light-blue"/>
             <span>{spanText}</span>
         </div>
     )
@@ -16,10 +16,10 @@ export function Input({inputName, placeholder="", type = 'text', spanText="", la
 
 Input.propTypes = {
     inputName: PropTypes.string.isRequired,
+    inputId: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
     type: PropTypes.string,
     spanText: PropTypes.string,
-    inputId: PropTypes.string.isRequired,
     labelText: PropTypes.string,
     required: PropTypes.string
 }
