ffe5760819ed1a6009b22b16059fd9065a148ed7
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-08-30 20:46:42 -0600

    Cards componets props update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { NavBar } from "./assets/components/NavBar";
 import { Footer } from "./assets/components/Footer";
-import { Input } from "./assets/components/Input";
-import { BasicCard } from "./assets/components/BasicCard";
-import { AdvancedCard } from './assets/components/Advancedcard';
-import { ActionButton } from "./assets/components/ActionButton";
-import { RedirectButton } from "./assets/components/RedirectButton";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Homepage } from './assets/pages/homepage';
 
