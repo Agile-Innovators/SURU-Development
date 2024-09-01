a01b99d2ede7b1712173e579a6b6d35be8cfb225
allyprz <ashleycr33@gmail.com>
2024-09-01 17:25:11 -0600

    feat: implement email-sent page

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "../pages/public/Homepage.jsx";
 import { Login } from "../pages/public/Login.jsx";
+import { EmailSent } from "../pages/public/EmailSent.jsx";
 import { Register } from "../pages/public/Register.jsx";
 import { PasswordReset } from "../pages/public/PasswordReset.jsx";
 
@@ -11,6 +12,7 @@ export function PublicRoutes() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/password-reset" element={<PasswordReset />} />
+            <Route path="/email-sent" element={<EmailSent />} />
             <Route path="/*" element={<Homepage />} />
         </Routes>
     );
