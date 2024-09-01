f893520e4549411059437f862200e14e48216902
allyprz <ashleycr33@gmail.com>
2024-09-01 15:54:49 -0600

    feat: implement login page

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "../pages/public/Homepage.jsx";
+import { Login } from "../pages/public/Login.jsx";
 
 export function PublicRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/*" element={<Homepage />} />
         </Routes>
     );
