9a81bdf2e3dac3de277f6a6229c1fabc3bfb04da
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-09-02 15:52:53 -0600

    landing page added

--- src/components/ui/RedirectButton.jsx
+++ src/components/ui/RedirectButton.jsx
@@ -1,20 +1,29 @@
-import { PropTypes } from 'prop-types';
+import { PropTypes } from "prop-types";
 
-export function RedirectButton({text, href , variant = 'fill',  customClass = ""}){
-
+export function RedirectButton({
+    text,
+    href,
+    variant = "fill",
+    customClass = "",
+}) {
     const variantClasses = {
-        fill: 'bg-secondary text-white hover:bg-light-blue hover:text-primary',
-        border: 'text-secondary border-2 border-secondary hover:bg-secondary hover:text-white'
-    }
+        fill: "bg-secondary text-white hover:bg-light-blue hover:text-primary",
+        border: "text-secondary border-2 border-secondary hover:bg-secondary hover:text-white",
+    };
 
-    return(
-        <a href={href} className={`block text-center px-8 py-3 rounded-md transition-colors duration-150 cursor-pointer ${variantClasses[variant]} ${customClass}`}>{text}</a>
-    )
+    return (
+        <a
+            href={href}
+            className={`block text-center px-8 py-3 rounded-md transition-colors duration-150 cursor-pointer ${variantClasses[variant]} ${customClass}`}
+        >
+            {text}
+        </a>
+    );
 }
 
 RedirectButton.propTypes = {
     text: PropTypes.string.isRequired,
     href: PropTypes.string.isRequired,
-    variant: PropTypes.oneOf(['fill', 'border']).isRequired,
-    customClass: PropTypes.string
-}
+    variant: PropTypes.oneOf(["fill", "border"]),
+    customClass: PropTypes.string,
+};
