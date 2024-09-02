9121c89cb1e9daa21777d43224782ae69ee02455
cap1112 <angelv.salazar11@gmail.com>
2024-09-01 20:35:33 -0600

    Componente de Selección de Acción
    Landing Page
    Se agrega un Focus en el Nav.

--- src/components/ui/BasicCard.jsx
+++ src/components/ui/BasicCard.jsx
@@ -2,9 +2,10 @@ import { PropTypes } from 'prop-types';
 
 //el parametro children permite que ESTE componente reciba una etiqueta html u otro componente de react
 //para que este componente reciba un children, debe de tener un etiqueta de apertura y cierre cuando se instancia
-export function BasicCard({children, src, title, text}){
+export function BasicCard({children, src, title, text, customClass = ""}){
+
     return(
-        <div className="flex flex-col items-center text-center gap-3 w-fit">
+        <div className={`flex flex-col items-center text-center gap-3 w-fit ${customClass}`}>
             <img src={`${src}`} alt="image card" />
             <h3>{title}</h3>
             <p>{text}</p>
@@ -18,4 +19,5 @@ BasicCard.propTypes = {
     src: PropTypes.string,
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
+    customClass: PropTypes.string
 }
