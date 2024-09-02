f92d90b35513f68e32c918638b8d2884561580a1
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-09-02 16:44:37 -0600

    routes updated with ROUTE-PATHS

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -7,20 +7,21 @@ import { EmailCode } from "../pages/public/EmailCode.jsx";
 import { ResetPassword } from "../pages/public/ResetPassword.jsx";
 import { ConfirmPassword } from "../pages/public/ConfirmPassword.jsx";
 import { ForgotPassword } from "../pages/public/ForgotPassword.jsx";
+import { ROUTE_PATHS } from "./index.js";
 
 export function PublicRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/email-code" element={<EmailCode />} />
-            <Route path="/email-sent" element={<EmailSent />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/confirm-password" element={<ConfirmPassword />} />
-            <Route path="/*" element={<Homepage />} />
-            <Route path="/home" element={<Homepage />} />
+            <Route path={ROUTE_PATHS.HOME} element={<Homepage />} />
+            <Route path={ROUTE_PATHS.LOGIN} element={<Login />} />
+            <Route path={ROUTE_PATHS.REGISTER} element={<Register />} />
+            <Route path={ROUTE_PATHS.FORGOT_PASSWORD} element={<ForgotPassword />} />
+            <Route path={ROUTE_PATHS.EMAIL_CODE} element={<EmailCode />} />
+            <Route path={ROUTE_PATHS.EMAIL_SEND} element={<EmailSent />} />
+            <Route path={ROUTE_PATHS.RESET_PASSWORD} element={<ResetPassword />} />
+            <Route path={ROUTE_PATHS.CONFIRM_PASSWORD} element={<ConfirmPassword />} />
+            <Route path={ROUTE_PATHS.NOT_FOUND} element={<Homepage />} />
+            <Route path={ROUTE_PATHS.HOME} element={<Homepage />} />
         </Routes>
     );
 }
