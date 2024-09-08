3c12c31278fcf6073b681cef6914223dc79d8460
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-09-08 14:07:56 -0600

    refactor: basic card hover added

--- src/components/ui/BasicCard.jsx
+++ src/components/ui/BasicCard.jsx
@@ -2,12 +2,12 @@ import { PropTypes } from 'prop-types';
 
 //el parametro children permite que ESTE componente reciba una etiqueta html u otro componente de react
 //para que este componente reciba un children, debe de tener un etiqueta de apertura y cierre cuando se instancia
-export function BasicCard({children, src, title, variant, text}){
+export function BasicCard({children, src, title, variant, text, customClass = ""}){
     const variantClasses = {
-        border: 'text-secondary border-2 border-gray rounded-md  p-6'
+        border: 'text-secondary border-2 border-gray rounded-md p-6'
       };
     return(
-        <div className={`flex flex-col items-center text-center gap-3 w-fit ${variantClasses[variant]}`} >
+        <div className={`flex flex-col items-center text-center gap-3 p-8 w-fit border border-light-grey rounded-md transition-all duration-300 hover:shadow-md ${customClass} ${variantClasses[variant]}`} >
             <img className='w-24' src={`${src}`} alt="image card" />
             <h3>{title}</h3>
             <p>{text}</p>
