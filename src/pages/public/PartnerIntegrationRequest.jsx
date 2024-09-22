80f88729b2dd85d0e129e046d3c2a5b08c5e6155
cap1112 <angelv.salazar11@gmail.com>
2024-09-22 17:54:36 -0600

    Continuación de Partner Integration

--- src/pages/public/PartnerIntegrationRequest.jsx
+++ src/pages/public/PartnerIntegrationRequest.jsx
@@ -1,10 +1,16 @@
 import { PartnerIntegrationRequestForm } from "../../components/activity/forms/PartnerIntegrationRequestForm";
 import { BackButton } from "../../components/ui/BackButton";
+import { SectionDivider } from "../../components/ui/SectionDivider";
 export function PartnerIntegrationRequest() {
   return (
-    <div >
+    <div className="mx-auto max-w-7xl px-6 lg:px-8 gap-2 mt-4">
         <BackButton />
-        <PartnerIntegrationRequestForm />
+        <h2 className="mt-10 text-center sm:text-start">Complementary Services Registration Form</h2>
+        <SectionDivider/>
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-center mx-auto max-w-7xl">
+          <PartnerIntegrationRequestForm />
+        </div>
+        
     </div>
   );
 }
