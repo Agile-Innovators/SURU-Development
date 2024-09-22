bc584a6c38f113912bcfc36fb2f1b1d522a5087f
Carlos Vasquez <115187830+ChillyGuaro@users.noreply.github.com>
2024-09-22 00:08:49 -0600

    Fixed other mistakes in properties form

--- src/components/activity/forms/CreatePropertyForm.jsx
+++ src/components/activity/forms/CreatePropertyForm.jsx
@@ -328,82 +328,110 @@ const CreatePropertyForm = () => {
         </form>
       ),
       "bare-land": (
-        <form onSubmit={handleSubmit}>
-          {/* Sección de detalles del terreno */}
-          <SectionDivider text="Bare Land details" />
-          <BaseFormsInfo />
+  <form onSubmit={handleSubmit}>
+    {/* Sección de detalles del terreno */}
+    <SectionDivider text="Bare Land details" />
+    <BaseFormsInfo />
 
-          {/* Input para el tamaño de la propiedad */}
-          <div className="grid grid-cols-2 gap-4 my-4">
-            <InputForms
-              inputName="size"
-              inputId="size"
-              type="number"
-              labelText="Size"
-              placeholder="Property size in square meters"
-            />
-          </div>
+    {/* Input para el tamaño de la propiedad */}
+    <div className="grid grid-cols-2 gap-4 my-4">
+      <InputForms
+        inputName="size"
+        inputId="size"
+        type="number"
+        labelText="Size"
+        placeholder="Property size in square meters"
+      />
+    </div>
 
-          {/* Servicios disponibles */}
-          <SectionDivider text="Available services" />
-          <div className="flex space-x-2">
-            {["availableWater", "availableElectricity"].map((service) => (
-              <MainButton
-                key={service}
-                onClick={() => toggleService(service)} // Función para manejar la selección de servicios disponibles
-                type="boolean"
-                variant="border"
-                isChecked={services[service]} // Verifica si el servicio está habilitado
-                customClass="capitalize"
-                text={service.replace("available", "")} // Mostrar texto sin el prefijo 'available'
-              />
-            ))}
-          </div>
+    {/* Servicios disponibles */}
+    <SectionDivider text="Available services" />
+    <div className="flex space-x-2">
+      {["availableWater", "availableElectricity"].map((service) => (
+        <MainButton
+          key={service}
+          onClick={() => toggleService(service)} // Función para manejar la selección de servicios disponibles
+          type="boolean"
+          variant="border"
+          isChecked={services[service]} // Verifica si el servicio está habilitado
+          customClass="capitalize"
+          text={service.replace("available", "")} // Mostrar texto sin el prefijo 'available'
+        />
+      ))}
+    </div>
 
-          {/* Mostrar la sección de "Include services" solo si "availableWater" o "availableElectricity" están seleccionados */}
-          {(services.availableWater || services.availableElectricity) && (
-            <>
-              {(accion === "rent" || accion === "both") && (
+    {/* Mostrar la sección de "Include services" solo si "availableWater" o "availableElectricity" están seleccionados */}
+    {(services.availableWater || services.availableElectricity) && (
+      <>
+        {(accion === "rent" || accion === "both") && (
+          <>
+            <SectionDivider text="Include services" />
+            <div className="grid grid-cols-2 gap-4 my-4">
+              {services.availableWater && (
+                <MainButton
+                  key="water"
+                  onClick={() => toggleService("water")}
+                  type="boolean"
+                  variant="border"
+                  isChecked={services["water"]}
+                  customClass="capitalize"
+                  text="Water"
+                />
+              )}
+              {services.availableElectricity && (
                 <>
-                  <SectionDivider text="Include services" />
-                  <div className="grid grid-cols-2 gap-4 my-4">
-                    {["water", "electricity", "wifi", "cable"].map(
-                      (service) => (
-                        <MainButton
-                          key={service}
-                          onClick={() => toggleService(service)}
-                          type="boolean"
-                          variant="border"
-                          isChecked={services[service]}
-                          customClass="capitalize"
-                          text={
-                            service.charAt(0).toUpperCase() + service.slice(1)
-                          }
-                        />
-                      )
-                    )}
-                  </div>
+                  <MainButton
+                    key="electricity"
+                    onClick={() => toggleService("electricity")}
+                    type="boolean"
+                    variant="border"
+                    isChecked={services["electricity"]}
+                    customClass="capitalize"
+                    text="Electricity"
+                  />
+                  <MainButton
+                    key="wifi"
+                    onClick={() => toggleService("wifi")}
+                    type="boolean"
+                    variant="border"
+                    isChecked={services["wifi"]}
+                    customClass="capitalize"
+                    text="WiFi"
+                  />
+                  <MainButton
+                    key="cable"
+                    onClick={() => toggleService("cable")}
+                    type="boolean"
+                    variant="border"
+                    isChecked={services["cable"]}
+                    customClass="capitalize"
+                    text="Cable"
+                  />
                 </>
               )}
-            </>
-          )}
+            </div>
+          </>
+        )}
+      </>
+    )}
 
-          {/* Mostrar secciones de precios según la acción seleccionada */}
-          {accion === "sale" && <PriceDetails type="Sale" />}
-          {accion === "rent" && <PriceDetails type="Rent" />}
-          {accion === "both" && <PriceDetails type="Both" />}
+    {/* Mostrar secciones de precios según la acción seleccionada */}
+    {accion === "sale" && <PriceDetails type="Sale" />}
+    {accion === "rent" && <PriceDetails type="Rent" />}
+    {accion === "both" && <PriceDetails type="Both" />}
 
-          {/* Botón para publicar */}
-          <MainButton
-            text="Publish"
-            onClick={handleSubmit}
-            type="submit"
-            disabled={loading}
-          >
-            {loading ? "Publishing..." : "Publish Property"}
-          </MainButton>
-        </form>
-      ),
+    {/* Botón para publicar */}
+    <MainButton
+      text="Publish"
+      onClick={handleSubmit}
+      type="submit"
+      disabled={loading}
+    >
+      {loading ? "Publishing..." : "Publish Property"}
+    </MainButton>
+  </form>
+),
+
 
       "retail-space": (
         <form onSubmit={handleSubmit}>
@@ -499,7 +527,6 @@ const CreatePropertyForm = () => {
           </div>
           <div>
             <SectionDivider text="Upload an image" />
-            {/* ///////////////////////////// */}
             <div className="image-upload-container">
               <label
                 for="file-input"
