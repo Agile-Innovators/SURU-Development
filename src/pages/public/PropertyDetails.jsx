9c809b3634775798d29cdca2f8ea9097dfa5b251
cap1112 <angelv.salazar11@gmail.com>
2024-10-06 23:50:44 -0600

    Agregar condicionales para los tipos de propiedad

--- src/pages/public/PropertyDetails.jsx
+++ src/pages/public/PropertyDetails.jsx
@@ -1,29 +1,6 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {
-    Navigation,
-    Pagination,
-    Scrollbar,
-    A11y,
-    Autoplay,
-} from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+
 import { AdvancedCard } from '../../components/ui/cards/AdvancedCard';
-import {
-    Bath,
-    MapPin,
-    BedDouble,
-    CarFront,
-    Trees,
-    PawPrint,
-    LandPlot,
-    Droplet,
-    Wifi,
-    Zap,
-    Tv,
-} from 'lucide-react';
+import { Bath, MapPin, BedDouble, CarFront, Trees, PawPrint, LandPlot, Droplet, Wifi, Zap, Tv, } from 'lucide-react';
 import { ROUTE_PATHS } from '../../routes';
 import { useAxios } from '../../components/hooks/useAxios.js';
 import { MainButton } from '../../components/ui/buttons/MainButton';
@@ -31,6 +8,8 @@ import { BackButton } from '../../components/ui/buttons/BackButton';
 import { useState, useEffect } from 'react';
 import { GlobalProvider } from '../../global/GlobalProvider.jsx';
 import { useFetchPropertyDetails } from "../../components/hooks/useFetchPropertyDetails.js";
+import { ImageCarrusel } from '../../components/activity/property_details_components/PropertyFeatures.jsx';
+import { PropertyFeatures } from '../../components/activity/property_details_components/Utilities.jsx';
 
 export function PropertyDetails() {
     const { propertyDetails, isLoadingPropsDetails } = useFetchPropertyDetails();
@@ -41,35 +20,12 @@ export function PropertyDetails() {
     return (
         <div className="mx-auto max-w-7xl px-6 lg:px-8 gap-4 my-4">
             <BackButton />
-            
-            <Swiper
-                modules={[Navigation, Pagination, A11y, Autoplay]}
-                pagination={{ clickable: true }}
-                autoplay={{ delay: 3000 }}
-                spaceBetween={10}
-                slidesPerView={1}
-                className="rounded-md mt-4"
-            >
-                {property.images && property.images.length > 0 ? (
-                    property.images.map((image) => {
-                        return (
-                            <SwiperSlide key={image.id}>
-                                <img className='h-[45vh] w-full object-cover rounded-md'
-                                    src={image.url}
-                                    alt="Property Image" />
-                            </SwiperSlide>
-                        );
-                    })
-                ) : (
-                    <p>No images available</p>
-                )}
-            </Swiper>
-
-
-
-
+            {/* Carrusel */}
+            <ImageCarrusel propertyTemp={property} />
+            {/* Fin Carrusel */}
             <div className="grid grid-cols-[1fr] sm:grid-cols-[2fr_1fr] gap-4 mt-6">
                 <div className="flex flex-col gap-2">
+                    {/* Inicio Detalles Generales */}
                     <h2>{property.title}</h2>
                     <div className="flex">
                         <MapPin />
@@ -78,117 +34,64 @@ export function PropertyDetails() {
                     <p>
                         {property.description}
                     </p>
-                    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-4">
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <BedDouble size={40} strokeWidth={1.5} />
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">
-                                    {property.bedrooms}
-                                </b>
-                                <p>bedrooms</p>
-                            </div>
-                        </div>
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <Bath size={40} strokeWidth={1.5} />
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">
-                                    {property.bathrooms}
-                                </b>
-                                <p>bathrooms</p>
-                            </div>
-                        </div>
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-stairs"><rect width="10" height="4" x="2" y="16" /><rect width="10" height="4" x="4" y="12" /><rect width="10" height="4" x="6" y="8" /><rect width="10" height="4" x="8" y="4" /><path d="M12 20h10V4h-4" /></svg>
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">
-                                    {property.floors}
-                                </b>
-                                <p>floors</p>
-                            </div>
-                        </div>
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="40"
-                                height="40"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="#FFFFFF"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="lucide lucide-waves"
-                            >
-                                <path d="M2 6c.6.5 1.2 1 2.5 1C7 7 7 5 9.5 5c2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
-                                <path d="M2 12c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
-                                <path d="M2 18c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
-                            </svg>
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">
-                                    {property.pools}
-                                </b>
-                                <p>pool</p>
-                            </div>
-                        </div>
-                        {/* <div className=" w-full h-full flex flex-col justify-between">
-                            <CarFront size={40} strokeWidth={1.5} />
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">
-                                    {propertyDetails.garages}
-                                </b>
-                                <p>garage</p>
-                            </div>
-                        </div> */}
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <Trees size={40} strokeWidth={1.5} />
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">2</b>
-                                <p>backyards</p>
-                            </div>
-                        </div>
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <PawPrint size={40} strokeWidth={1.5} />
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">Allows</b>
-                                <p>pets</p>
-                            </div>
-                        </div>
-                        <div className=" w-full h-full flex flex-col justify-between">
-                            <LandPlot size={40} strokeWidth={1.5} />
-                            <div className="flex items-baseline space-x-1">
-                                <b className="font-bold">{property.size_in_m2}</b>
-                                <p>m²</p>
-                            </div>
-                        </div>
-                    </div>
+                    {/* Fin Detalles Generales */}
+                    {/* Inicio Features*/}
+                    <PropertyFeatures property={property} />
+                    {/* Fin Features*/}
                     <div className="flex flex-col gap-1 mt-5">
                         <h3>Property Details</h3>
                         <p><b>Availability Date:</b>  {property.availability_date}.</p>
-
+                        <p><b>Property Category:</b> {property.property_category}</p>
                         {/* <p><b>Specific Direction:</b> 175 meters north of Tierra Mia Restaurant, near Arenal Volcano National Park.</p> */}
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col border-2 gap-2 rounded-md p-4">
                         <h3>Pricing details</h3>
-
                         {/* este if verifica el tipo de propiedad */}
                         {property.property_transaction === "Sale" ? (
                             <div className='flex justify-between'>
                                 <p>Sale Payment</p>
                                 <p className="font-medium">{property.currency_code} {property.price}</p>
                             </div>
-                        ) : (
+                        ) : property.property_transaction === "Rent" ? (
                             <div>
                                 <div className="flex justify-between">
                                     <p>Rental Price</p>
+                                    <p className="font-medium">{property.currency_code} {property.rent_price} {property.payment_frequency}</p>
+                                </div>
+                                {property.deposit_price && property.deposit_price > 0 ? (
+                                    <div className="flex justify-between">
+                                        <p>Security Deposit</p>
+                                        <p className="font-medium">{property.currency_code} {property.deposit_price}</p>
+                                    </div>
+                                ) : (
+                                    <p className="font-medium">No security deposit available</p>
+                                )}
+                            </div>
+                        ) : property.property_transaction === "Dual" ? (
+                            <div>
+
+                                <div className="flex justify-between">
+                                    <p>Sale Payment</p>
                                     <p className="font-medium">{property.currency_code} {property.price}</p>
                                 </div>
-                                <div className="flex justify-between">
-                                    <p>Segurity Deposit</p>
-                                    <p className="font-medium">{property.currency_code} </p>
+                                <div className="border border-b-1 m-2"></div>
+                                <div className="flex justify-between mt-2">
+                                    <p>Rental Price</p>
+                                    <p className="font-medium">{property.currency_code} {property.rent_price} {property.payment_frequency}</p>
                                 </div>
+                                {property.deposit_price && property.deposit_price > 0 ? (
+                                    <div className="flex justify-between">
+                                        <p>Security Deposit</p>
+                                        <p className="font-medium">{property.currency_code} {property.deposit_price}</p>
+                                    </div>
+                                ) : (
+                                    <p className="font-medium">No security deposit available</p>
+                                )}
                             </div>
+                        ) : (
+                            <p className="font-medium">Transaction type not available</p>
                         )}
 
                         <MainButton
@@ -199,62 +102,7 @@ export function PropertyDetails() {
                         />
                         <MainButton text="Get Property" type="button" />
                     </div>
-                    <div className="flex flex-col border-2 rounded-md p-4 gap-2">
-                        <h3>Included Utilities</h3>
 
-                        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2'>
-                            {property.utilities && property.utilities.length > 0 ? (
-                                property.utilities.map((utility) => {
-                                    switch (utility.name) {
-                                        case 'Water':
-                                            return (
-                                                <div
-                                                    key={utility.id}
-                                                    className="flex flex-col items-center gap-2"
-                                                >
-                                                    <Droplet />
-                                                    <p>Water</p>
-                                                </div>
-                                            );
-                                        case 'Wifi':
-                                            return (
-                                                <div
-                                                    key={utility.id}
-                                                    className="flex flex-col items-center gap-2"
-                                                >
-                                                    <Wifi />
-                                                    <p>Wi-Fi</p>
-                                                </div>
-                                            );
-                                        case 'Electricity':
-                                            return (
-                                                <div
-                                                    key={utility.id}
-                                                    className="flex flex-col items-center gap-2"
-                                                >
-                                                    <Zap />
-                                                    <p>Electricity</p>
-                                                </div>
-                                            );
-                                        case 'Furnished':
-                                            return (
-                                                <div
-                                                    key={utility.id}
-                                                    className="flex flex-col items-center gap-2"
-                                                >
-                                                    <Tv />
-                                                    <p>Furnished</p>
-                                                </div>
-                                            );
-                                        default:
-                                            return null;
-                                    }
-                                })
-                            ) : (
-                                <p>No utilities available</p>
-                            )}
-                        </div>
-                    </div>
                 </div>
             </div>
 
