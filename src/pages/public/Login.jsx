31e861d59afab3382c3bd21d172cef491a57deab
ashleyrojas <ashleycr33@gmail.com>
2024-10-28 11:15:23 -0600

    feat: reset password trough email functionality

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -2,9 +2,12 @@ import { LoginForm } from '../../components/activity/forms/LoginForm.jsx';
 import { forceLightMode } from '../../components/hooks/utils.js';
 import { ThemeContext } from '../../global/ThemeContext.jsx';
 import { useEffect, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export function Login() {
     const { theme } = useContext(ThemeContext);
+    const location = useLocation();
+    const successMessage = location.state?.message || '';
 
     useEffect(() => {
         forceLightMode(theme);
@@ -13,7 +16,7 @@ export function Login() {
     return (
         <div className="bg-authentication grid">
             <div className="max-w-7xl mx-auto p-5 grid content-center">
-                <LoginForm />
+            <LoginForm successMessage={successMessage} />
             </div>
         </div>
     );
