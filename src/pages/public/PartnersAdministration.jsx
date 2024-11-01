0d0e7ba775cb38dfb77af33fa5e97a1eb7f63db1
Isaac Corella Alvarado <143538498+IsaacCorellaA@users.noreply.github.com>
2024-10-31 21:36:50 -0600

    Partners Admin completed

--- src/pages/public/PartnersAdministration.jsx
+++ src/pages/public/PartnersAdministration.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Tabs, Tab, Box, Button } from '@mui/material';
-import { motion, AnimatePresence } from 'framer-motion';
-import { useAxios } from '../../components/hooks/useAxios';
+import React, { useState, useEffect } from 'react';
+import { Tabs, Tab, Box } from '@mui/material';
 import Swal from 'sweetalert2';
-
+import { useAxios } from '../../components/hooks/useAxios';
 
 export function PartnersAdministration() {
     const user = JSON.parse(localStorage.getItem('user')) || null;
@@ -14,16 +12,15 @@ export function PartnersAdministration() {
     const [loading, setLoading] = useState(true);
     const axios = useAxios();
 
-
     const fetchPartners = (currentStatus) => {
         if (!loggedInUserId) {
             console.error("User ID is not available.");
             return;
         }
-    
+   
         setLoading(true);
         axios
-            .get(`/partner-requests/${currentStatus}/${loggedInUserId}`) 
+            .get(`/partner-requests/${currentStatus}/${loggedInUserId}`)
             .then((response) => {
                 console.log("Fetched Partners:", response.data);
                 setPartners(response.data || []);
@@ -31,12 +28,9 @@ export function PartnersAdministration() {
             })
             .catch((error) => {
                 console.error("Error fetching partners:", error.response ? error.response.data : error.message);
-                Swal.fire('Error!', 'Failed to load partners', 'error');
                 setLoading(false);
             });
     };
-
-
 
     useEffect(() => {
         fetchPartners(currentStatus);
@@ -90,75 +84,102 @@ export function PartnersAdministration() {
 
     return (
         <div className="max-w-7xl m-auto p-4 min-h-[80vh]">
-            <h1 className="mt-10">Partners Administration</h1>
-            <p className="dark:text-white">Manage partner requests and status</p>
+            <div className="mt-10 mb-10 gap-4 sm:block">
+                <div className="m-auto p-4">
+                    <h1 className="mt-10">Partners Administration</h1>
 
-            <Box className="flex justify-between items-center mt-4 mb-4">
-                <Tabs
-                    value={currentStatus}
-                    onChange={handleTabChange}
-                    variant="scrollable"
-                    scrollButtons="auto"
-                    aria-label="status tabs"
-                >
-                    <Tab label="Pending" value="Pending" className="dark:text-white" />
-                    <Tab label="Approved" value="Approved" className="dark:text-white" />
-                    <Tab label="Rejected" value="Rejected" className="dark:text-white"/>
-                </Tabs>
-            </Box>
+                    <Box className="flex justify-between items-center mt-4 mb-4">
+                        <Tabs
+                            value={currentStatus}
+                            onChange={handleTabChange}
+                            variant="scrollable"
+                            scrollButtons="auto"
+                            aria-label="status tabs"
+                            className="flex-1"
+                        >
+                            <Tab label="Pending" value="Pending" />
+                            <Tab label="Approved" value="Approved" />
+                            <Tab label="Rejected" value="Rejected" />
+                        </Tabs>
+                    </Box>
 
-            <div className="grid grid-cols-1 gap-4 mt-4">
-                {loading ? (
-                    <p className="text-center text-gray-600 font-bold">Loading...</p>
-                ) : partners.length === 0 ? (
-                    <p className="text-center text-gray-600 font-bold">No partners found in this category.</p>
-                ) : (
-                    <AnimatePresence>
+                    {/* Tabla para pantallas grandes */}
+                    <div className="overflow-auto rounded-lg shadow hidden md:block">
+                        <table className="w-full">
+                            <thead className="bg-gray-300 border-b-2 border-gray-200 text-black text-left">
+                                <tr>
+                                    <th className="p-3 text-sm font-semibold tracking-wide text-center">Company Name</th>
+                                    <th className="w-32 p-3 text-sm font-semibold tracking-wide">Category</th>
+                                    <th className="p-3 text-sm font-semibold tracking-wide">Phone</th>
+                                    <th className="p-3 text-sm font-semibold tracking-wide text-center">Email Address</th>
+                                    <th className="w-20 p-3 text-sm font-semibold tracking-wide">Status</th>
+                                    <th className="p-3 text-sm font-semibold tracking-wide text-center">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-300">
+                                {partners.map((partner) => (
+                                    <tr className="bg-white" key={partner.id}>
+                                        <td className="p-3 text-sm text-gray-700 whitespace-nowrap flex items-center gap-2">
+                                            <img
+                                                src={partner.image_url}
+                                                alt="Company Logo"
+                                                className="w-10 h-10 rounded-full bg-gray-200"
+                                            />
+                                            {partner.name}
+                                        </td>
+                                        <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{partner.category_name || 'N/A'}</td>
+                                        <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{partner.phone_number || 'N/A'}</td>
+                                        <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{partner.email || 'N/A'}</td>
+                                        <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                                            <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-yellow-800 bg-yellow-200 rounded-lg opacity-50">{currentStatus}</span>
+                                        </td>
+                                        <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                                            <div className="flex gap-2">
+                                                <button className="text-secondary border-2 border-secondary hover:bg-secondary dark:border-light-blue dark:text-light-blue hover:text-white py-3 px-3" onClick={() => approvePartner(partner.id)}>Approve</button>
+                                                <button className="text-secondary border-2 border-secondary hover:bg-secondary dark:border-light-blue dark:text-light-blue hover:text-white py-3 px-3" onClick={() => rejectPartner(partner.id)}>Reject</button>
+                                                <button className="text-secondary border-2 border-secondary hover:bg-secondary dark:border-light-blue dark:text-light-blue hover:text-white py-3 px-3">Download PDF</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {/* Diseño responsive para pantallas pequeñas */}
+                    <div className="grid grid-cols-1 gap-4 md:hidden">
                         {partners.map((partner) => (
-                            <motion.div
-                                key={partner.id}
-                                initial={{ opacity: 0, scale: 0.8 }}
-                                animate={{ opacity: 1, scale: 1 }}
-                                exit={{ opacity: 0, scale: 0.8 }}
-                                transition={{ duration: 0.3 }}
-                                className="w-full border border-gray-300 rounded-md p-4 flex flex-col sm:flex-row items-center sm:justify-between gap-4"
-                            >
-                                <div className="flex items-center gap-4">
-                                    <div className="w-12 h-12 rounded-full overflow-hidden bg-gray-200">
-                                        {partner.image_url && <img src={partner.image_url} alt={`${partner.name} Logo`} />}
-                                    </div>
-                                    <div>
-                                        <h2 className="font-bold">{partner.name}</h2>
-                                        <p className="text-gray-600">{partner.category_name}</p>
-                                    </div>
+                            <div className="bg-white p-4 rounded-lg shadow overflow-hidden" key={partner.id}>
+                                <div className="flex items-center space-x-2 text-sm">
+                                    <img
+                                        src={partner.image_url}
+                                        alt="Company Logo"
+                                        className="w-10 h-10 rounded-full"
+                                    />
+                                    <div className="font-bold">{partner.name}</div>
+                                    <div className="p-1.5 text-xs font-medium uppercase tracking-wider text-yellow-800 bg-yellow-200 rounded-lg opacity-50">{currentStatus}</div>
                                 </div>
-
-                                <div className="flex-1 text-gray-600 text-left sm:text-center">
-                                    <p>{partner.description || "No description available"}</p>
+                                <div className="flex gap-2 text-sm mt-2">
+                                    <label className="font-bold">Service:</label>
+                                    <p>{partner.category_name || 'N/A'}</p>
                                 </div>
-
-                                <div className="relative gap-5 flex">
-                                    {currentStatus === "Pending" && (
-                                        <>
-                                            <button
-                                                className="text-green-500 border border-green-500 hover:bg-green-500 hover:text-white py-1 px-3 rounded"
-                                                onClick={() => approvePartner(partner.id)}
-                                            >
-                                                Approve
-                                            </button>
-                                            <button
-                                                className="text-red-500 border border-red-500 hover:bg-red-500 hover:text-white py-1 px-3 rounded"
-                                                onClick={() => rejectPartner(partner.id)}
-                                            >
-                                                Reject
-                                            </button>
-                                        </>
-                                    )}
+                                <div className="flex gap-2 text-sm mt-2">
+                                    <label className="font-bold">Phone Number:</label>
+                                    <p>{partner.phone_number || 'N/A'}</p>
                                 </div>
-                            </motion.div>
+                                <div className="flex gap-2 text-sm mt-2">
+                                    <label className="font-bold">Email:</label>
+                                    <p>{partner.email || 'N/A'}</p>
+                                </div>
+                                <div className="flex gap-2 mt-2 text-sm">
+                                    <button className="text-secondary border-2 border-secondary hover:bg-secondary dark:border-light-blue dark:text-light-blue hover:text-white py-3 px-3" onClick={() => approvePartner(partner.id)}>Approve</button>
+                                    <button className="text-secondary border-2 border-secondary hover:bg-secondary dark:border-light-blue dark:text-light-blue hover:text-white py-3 px-3" onClick={() => rejectPartner(partner.id)}>Reject</button>
+                                    <button className="text-secondary border-2 border-secondary hover:bg-secondary dark:border-light-blue dark:text-light-blue hover:text-white py-3 px-3">Download PDF</button>
+                                </div>
+                            </div>
                         ))}
-                    </AnimatePresence>
-                )}
+                    </div>
+                </div>
             </div>
         </div>
     );
