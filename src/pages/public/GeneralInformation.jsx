0ef3e51f90cdf275fc2ded2f02bb3e90d33ce6ea
ashleyrojas <ashleycr33@gmail.com>
2024-11-04 11:39:47 -0600

    fix: correcting user general information update method

--- src/pages/public/GeneralInformation.jsx
+++ src/pages/public/GeneralInformation.jsx
@@ -7,8 +7,9 @@ import { Pencil } from 'lucide-react';
 import { useFetchLocations } from '../../components/hooks/useFetchLocations';
 import { Bounce, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATHS } from '../../routes';
+import { useAxios } from '../../components/hooks/useAxios.js';
 
 export function GeneralInformation() {
     const { getUser } = useAuth();
@@ -16,8 +17,8 @@ export function GeneralInformation() {
     const { updateUserProfile, getUserInformation, loading, error, data } = useFetchUser();
     const [userData, setUserData] = useState(null);
     const { locations } = useFetchLocations();
+    const axios = useAxios();
     const navigate = useNavigate();
-
 
     if (user.user_type == "partner") {
         navigate(ROUTE_PATHS.HOME);
@@ -32,8 +33,8 @@ export function GeneralInformation() {
         email: '',
         phone_number: '',
         image:
-            user.image_url ||
-            'https://res.cloudinary.com/dvwtm566p/image/upload/v1728158504/users/dc8aagfamyqwaspllhz8.jpg',
+        user.image_url ||
+        'https://res.cloudinary.com/dvwtm566p/image/upload/v1728158504/users/dc8aagfamyqwaspllhz8.jpg',
     });
 
     const [isEditing, setIsEditing] = useState(false); // Estado para controlar si se está editando o no
@@ -109,44 +110,132 @@ export function GeneralInformation() {
     const handleProfileSubmit = async (e) => {
         e.preventDefault();
         if (user?.id) {
+            // Crear un objeto FormData y llenarlo con los datos del perfil
             const formData = new FormData();
-
             for (const key in profileData) {
-                if (profileData[key]) {
-                    formData.append(key, profileData[key]);
+                formData.append(key, profileData[key]);
+            }
+            formData.append('_method', 'PUT'); // Método PUT para la compatibilidad con Laravel
+    
+            try {
+                const response = await axios.post(`user/update/${user.id}`, formData);
+    
+                // Verificar el estado de la respuesta
+                if (response.status !== 200) {
+                    toast.error('Error updating information', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        transition: Bounce,
+                    });
+                    console.error('Update failed:', response);
+                    return;
                 }
+    
+                // Actualizar la información del usuario con la respuesta
+                const updatedUser = response.data;
+                console.log('User updated:', updatedUser);
+    
+                if (updatedUser?.image_url) {
+                    setProfileData((prevState) => ({
+                        ...prevState,
+                        image: updatedUser.image_url,
+                    }));
+                }
+    
+                // Refrescar la información del usuario
+                getUserInformation(user.id);
+                toast.success('Information updated successfully', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                });
+                setIsEditing(false);
+            } catch (error) {
+                console.error('Update failed:', error);
+                toast.error('Error updating information', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                });
             }
-
-            formData.append('_method', 'PUT');
-            // console.log([...formData]);
-            const updatedUser = await updateUserProfile(user.id, formData);
-
-            // Actualizar el estado de profileData con la nueva imagen
-            if (updatedUser?.image_url) {
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    image: updatedUser.image_url,
-                }));
-            }
-
-            // Actualizar los datos del usuario
-            getUserInformation(user.id);
-            toast.success('Information updated successfully', {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-                transition: Bounce,
-            });
-
-
         }
-        setIsEditing(false);
     };
+    
+
+    // const handleProfileSubmit = async (e) => {
+    //     e.preventDefault();
+    //     if (user?.id) {
+    //         const formData = new FormData();
+
+    //         for (const key in profileData) {
+    //             if (profileData[key]) {
+    //                 formData.append(key, profileData[key]);
+    //             }
+    //         }
+
+    //         formData.append('_method', 'PUT');
+    //         console.log([...formData]);
+    //         const updatedUser = await updateUserProfile(user.id, formData);
+
+    //         if (updatedUser && updatedUser.error) {
+    //             toast.error('Error updating information', {
+    //                 position: "top-center",
+    //                 autoClose: 5000,
+    //                 hideProgressBar: false,
+    //                 closeOnClick: true,
+    //                 pauseOnHover: true,
+    //                 draggable: true,
+    //                 progress: undefined,
+    //                 theme: "light",
+    //                 transition: Bounce,
+    //             });
+    //             return; // Detener la ejecución aquí si hay un error
+    //         }
+
+    //         // Actualizar el estado de profileData con la nueva imagen
+    //         if (updatedUser?.image_url) {
+    //             setProfileData((prevState) => ({
+    //                 ...prevState,
+    //                 image: updatedUser.image_url,
+    //             }));
+    //         }
+
+    //         // Actualizar los datos del usuario
+    //         getUserInformation(user.id);
+    //         toast.success('Information updated successfully', {
+    //             position: "top-center",
+    //             autoClose: 5000,
+    //             hideProgressBar: false,
+    //             closeOnClick: true,
+    //             pauseOnHover: true,
+    //             draggable: true,
+    //             progress: undefined,
+    //             theme: "light",
+    //             transition: Bounce,
+    //         });
+
+
+    //     }
+    //     setIsEditing(false);
+    // };
 
     const handleEditClick = () => {
         setIsEditing((prev) => !prev);
