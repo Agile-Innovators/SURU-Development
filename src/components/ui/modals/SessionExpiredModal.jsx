d8b28112fad3afe26a957d99fce9c9abf24feae6
Carlos Vasquez <115187830+ChillyGuaro@users.noreply.github.com>
2024-11-15 19:40:42 -0600

    fixed darkmde sesion expired

--- src/components/ui/modals/SessionExpiredModal.jsx
+++ src/components/ui/modals/SessionExpiredModal.jsx
@@ -1,9 +1,10 @@
+
 import { Clock } from 'lucide-react';
 
 export function SessionExpiredModal() {
     return (
-        <div className="bg-white p-5 rounded shadow-lg text-center m-auto max-w-[25rem]">
-            <div className="m-auto p-3 bg-translucid-blue rounded-full inline-block">
+        <div className="bg-white dark:bg-gray-800 p-5 rounded shadow-lg text-center m-auto max-w-[25rem]">
+            <div className="m-auto p-3 bg-translucid-blue dark:bg-[#045B80]  rounded-full inline-block">
                 <Clock size={42} className="text-secondary stroke-2" />
             </div>
             <h2 className="text-xl font-semibold my-4">Session has expired</h2>
